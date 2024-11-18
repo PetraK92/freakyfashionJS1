c2b6c44ba1d67f12c6e70ac43715e7a462d8f68e
PetraK92 <petrakarlsson92@hotmail.com>
2024-11-18 09:35:01 +0100

    Första commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+// const products =  require('../api.js');
+const db = require("../db/db");
+
+// // lagt till själv
+// const Database = require('sqlite3');
+// const db = new Database('./db/freakyfashion.db', { verbose: console.log });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+const sql =`
+  SELECT name,
+        price,
+        brand,
+        image,
+        addedDate,
+        slug
+    FROM products
+`;
+
+db.all(sql, [], function(error, rows) {
+  console.log("rows", rows);
+  res.render('index', { 
+    title: 'Freaky fashion', 
+    products: rows
+  });
+});
+
 });
 
 module.exports = router;
